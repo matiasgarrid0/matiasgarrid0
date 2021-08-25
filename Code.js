371ae0e840e13f0895595769a299dd05e6fe099c
Matias Garrido <81318237+matiasgarrid0@users.noreply.github.com>
2021-08-25 04:03:28 -0300

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,1 +1,8 @@
-
+const matiasGarrido = {
+  Lenguajes de Programacion: [Javascript],
+  Desarrollo Frontend: [React, HTML, CSS, Bootstrap, Redux],
+  Desarrollo Backend: [Node],
+  Base de datos: [MySQL, PostgresSQL],
+  Software: [Ai, Ps, Invision, Postman]
+  Otros: [Git]
+}
